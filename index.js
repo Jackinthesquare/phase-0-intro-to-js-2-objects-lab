c865a0d03eae8693ae080bf126a9654bd061bf28
Jacky Liang <asheriffy@gmail.com>
2022-03-21 11:12:17 -0400

    commit tree 396986f781b1d4af67509cb1e09104d1cb54d804

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {name: 'Sam',
+                  streetAddress: '123 Fake St'};
+const updateEmployeeWithKeyAndValue = () => {
+    const newEmployee = {...employee}
+    newEmployee.streetAddress = '11 Broadway'
+    return newEmployee
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = () => {
+    employee.streetAddress = '12 Broadway'
+    return employee
+}
+
+const deleteFromEmployeeByKey = () => {
+    const newEmployee = {...employee}
+    delete newEmployee.name
+    return newEmployee
+}
+
+const destructivelyDeleteFromEmployeeByKey = () => {
+    delete employee.name
+    return employee
+}
